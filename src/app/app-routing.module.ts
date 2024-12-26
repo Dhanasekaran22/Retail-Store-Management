f2f38bb5d068302a6d48f0000ec3f204264914ef
Dhanasekaran22 <dhanasekaran2622@gmail.com>
2024-12-26 18:08:16 +0530

    order-summary component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { SelectedImageService } from './selected-image.service';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductSummaryService } from './product-summary.service';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 const routes: Routes = [
   {path:'',component:ProductListComponent},
-  {path:'image-details',component:ProductDetailsComponent}
+  {path:'image-details',component:ProductDetailsComponent},
+  {path:'order-summary',component:OrderSummaryComponent}
 ];
 
 @NgModule({
--- src/app/order-summary/order-summary.component.ts
+++ src/app/order-summary/order-summary.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductSummaryService } from '../product-summary.service';
 
 @Component({
   selector: 'app-order-summary',
   templateUrl: './order-summary.component.html',
   styleUrls: ['./order-summary.component.css']
 })
-export class OrderSummaryComponent {
+export class OrderSummaryComponent implements OnInit{
+  productData: any;
+
+  constructor(private summaryService:ProductSummaryService){}
+
+  ngOnInit(){
+    this.productData=this.summaryService.getProductData();
+  }
 
 }
